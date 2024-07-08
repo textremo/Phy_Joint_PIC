0c63ce9d0d579614bc5cde043b02ab048a22a0ba
whatshow <wfwfpwefe2323@gmail.com>
2024-07-08 15:48:59 +0800

    JPIC detect: update for 2nd version

--- JPIC.m
+++ JPIC.m
@@ -235,6 +235,196 @@ classdef JPIC < handle
         @No:            the noise (linear) power
         @sym_map(opt):  false by default. If true, the output will be mapped to the constellation
         %}
+        % function [x, H] = detect(self, Y_DD, lmax, kmax, No, varargin)
+        %     % register optional inputs 
+        %     inPar = inputParser;
+        %     addParameter(inPar,"sym_map", false, @(x) isscalar(x)&islogical(x));
+        %     inPar.KeepUnmatched = true;
+        %     inPar.CaseSensitive = false;
+        %     parse(inPar, varargin{:});
+        %     sym_map = inPar.Results.sym_map;
+        % 
+        %     % input check
+        %     [Y_DD_N, Y_DD_M] = size(Y_DD);
+        %     if Y_DD_N ~= self.N || Y_DD_M ~= self.M
+        %         error("The received matrix is not in the shape of (%d, %d).", self.N, self.M);
+        %     end
+        %     if ~isscalar(No)
+        %         error("The noise power is not a scalar.");
+        %     end
+        % 
+        %     % constant values
+        %     y = Y_DD.';
+        %     y = y(:);
+        %     xp = self.Xp.';
+        %     xp = xp(:);
+        %     xdlocs = self.XdLocs.';
+        %     xdlocs = xdlocs(:);
+        % 
+        %     % iterative detection
+        %     Xe = zeros(self.N, self.M);
+        %     ise_dsc_prev = zeros(self.sig_len, 1);
+        %     x_bse = zeros(self.sig_len, 1);
+        %     v_bse = zeros(self.sig_len, 1);
+        %     x_dsc = zeros(self.sig_len, 1);
+        %     v_dsc = zeros(self.sig_len, 1);
+        %     x_det = zeros(self.sig_len, 1);
+        %     for iter_id = 1:self.iter_num
+        %         % CE
+        %         X = Xe + self.Xp;
+        %         Phi = self.buildPhi(X, lmax, kmax);
+        %         h = inv(Phi'*Phi)*Phi'*y;
+        %         % build the channel
+        %         H = self.buildHdd(h, lmax, kmax);
+        %         [~, x_num] = size(H);
+        %         Ht = H';
+        %         Hty = Ht*y;
+        %         HtH = Ht*H;
+        %         HtH_off = ((eye(x_num)+1) - eye(x_num).*2).*HtH;
+        %         HtH_off_sqr = HtH_off.^2;
+        %         % SD
+        %         % SD - BSO
+        %         % SD - BSO - mean
+        %         if iter_id == 1
+        %             % SD - BSO - mean - 1st iter
+        %             switch self.sd_bso_mean_cal_init
+        %                 case self.SD_BSO_MEAN_CAL_INIT_MMSE
+        %                     bso_zigma_1 = inv(HtH + No/self.es*eye(x_num));
+        %                 case self.SD_BSO_MEAN_CAL_INIT_MRC
+        %                     bso_zigma_1 = diag(1./vecnorm(H).^2);
+        %                 case self.SD_BSO_MEAN_CAL_INIT_LS
+        %                     bso_zigma_1 = inv(HtH);
+        %                 otherwise
+        %                     bso_zigma_1 = eye(x_num);
+        %             end
+        %             x_bso = bso_zigma_1*(Hty - HtH_off*x_dsc);
+        %         else
+        %             % SD - BSO - mean - other iteration
+        %             switch self.sd_bso_mean_cal
+        %                 case self.SD_BSO_MEAN_CAL_MRC
+        %                     bso_zigma_n = diag(1./vecnorm(H).^2);
+        %                 case self.SD_BSO_MEAN_CAL_LS
+        %                     bso_zigma_n = inv(HtH);
+        %             end
+        %             x_bso = bso_zigma_n*(Hty - HtH_off*x_dsc);
+        %         end
+        %         % SD - BSO - variance
+        %         switch self.sd_bso_var_cal
+        %             case self.SD_BSO_VAR_CAL_MMSE
+        %                 bso_var_mat = diag(inv(HtH + No/self.es*eye(x_num)));
+        %             case self.SD_BSO_VAR_CAL_MRC
+        %                 bso_var_mat = 1./vecnorm(H).^2.';
+        %             case self.SD_BSO_VAR_CAL_LS
+        %                 bso_var_mat = diag(inv(HtH));
+        %         end
+        %         bso_var_mat_sqr = bso_var_mat.^2;
+        %         if self.sd_bso_var == self.SD_BSO_VAR_TYPE_APPRO
+        %             v_bso = No.*bso_var_mat;
+        %         end
+        %         if self.sd_bso_var == self.SD_BSO_VAR_TYPE_ACCUR
+        %             v_bso = No.*bso_var_mat + HtH_off_sqr*v_dsc.*bso_var_mat_sqr;
+        %         end
+        %         v_bso = max(v_bso, self.min_var);
+        % 
+        %         % SD - BSE
+        %         bse_x_bso_mat = repmat(x_bso(xdlocs), 1, self.constel_len);
+        %         bse_constel_mat = repmat(self.constel, self.data_len, 1) + repmat(xp(xdlocs), 1, self.constel_len);
+        %         % BSE - Estimate P(x|y) using Gaussian distribution
+        %         pxyPdfExpPower = -1./(2*v_bso(xdlocs)).*abs(bse_x_bso_mat - bse_constel_mat).^2;
+        %         pxypdfExpNormPower = pxyPdfExpPower - max(pxyPdfExpPower, [], 2);   % make every row the max power is 0
+        %         pxyPdf = exp(pxypdfExpNormPower);
+        %         % BSE - Calculate the coefficient of every possible x to make the sum of all
+        %         pxyPdfCoeff = 1./sum(pxyPdf, 2);
+        %         pxyPdfCoeff = repmat(pxyPdfCoeff, 1, self.constel_len);
+        %         % BSE - PDF normalisation
+        %         pxyPdfNorm = pxyPdfCoeff.*pxyPdf;
+        %         % BSE - calculate the mean and variance
+        %         x_bse(xdlocs) = sum(pxyPdfNorm.*self.constel, 2);
+        %         x_bse_mat = repmat(x_bse(xdlocs), 1, self.constel_len);
+        %         v_bse(xdlocs) = sum(abs(x_bse_mat - self.constel).^2.*pxyPdfNorm, 2);
+        %         v_bse(xdlocs) = max(v_bse(xdlocs), self.min_var);
+        %         % BSE - to original size
+        % 
+        %         % SD - DSC
+        %         switch self.sd_dsc_ise
+        %             case self.SD_DSC_ISE_MMSE
+        %                 dsc_w = inv(HtH + No/self.es*eye(x_num));
+        %             case self.SD_DSC_ISE_MRC
+        %                 dsc_w = diag(1./vecnorm(H).^2);
+        %             case self.SD_DSC_ISE_LS
+        %                 dsc_w = inv(HtH);
+        %         end
+        %         ise_dsc = (dsc_w*(Hty - HtH*(x_bse + xp))).^2;
+        %         ies_dsc_sum = ise_dsc + ise_dsc_prev;
+        %         ies_dsc_sum = max(ies_dsc_sum, self.min_var);
+        %         % DSC - rho (if we use this rho, we will have a little difference)
+        %         rho_dsc = ise_dsc_prev./ies_dsc_sum;
+        %         % DSC - mean
+        %         if iter_id == 1
+        %             x_dsc = x_bse;
+        %         else
+        %             if self.sd_dsc_mean_prev_sour == self.SD_DSC_MEAN_PREV_SOUR_BSE
+        %                 %x_dsc = ise_dsc./ies_dsc_sum.*x_bse_prev + ise_dsc_prev./ies_dsc_sum.*x_bse;
+        %                 x_dsc = (1 - rho_dsc).*x_bse_prev + rho_dsc.*x_bse;
+        %             end
+        %             if self.sd_dsc_mean_prev_sour == self.SD_DSC_MEAN_PREV_SOUR_DSC
+        %                 x_dsc = (1 - rho_dsc).*x_dsc + rho_dsc.*x_bse;
+        %             end
+        %         end
+        %         % DSC - variance
+        %         if iter_id == 1
+        %             v_dsc = v_bse;
+        %         else
+        %             if self.sd_dsc_var_prev_sour == self.SD_DSC_VAR_PREV_SOUR_BSE
+        %                 %v_dsc = ise_dsc./ies_dsc_sum.*v_bse_prev + ise_dsc_prev./ies_dsc_sum.*v_bse;
+        %                 v_dsc = (1 - rho_dsc).*v_bse_prev + rho_dsc.*v_bse;
+        %             end
+        %             if self.sd_dsc_var_prev_sour == self.SD_DSC_VAR_PREV_SOUR_DSC
+        %                 v_dsc = (1 - rho_dsc).*v_dsc + rho_dsc.*v_bse;
+        %             end
+        %         end
+        % 
+        %         % early stop
+        %         if iter_id > 1 && sum(abs(v_dsc - v_dsc_prev).^2) <= self.iter_diff_min
+        %             break;
+        %         end
+        % 
+        %         % update statistics
+        %         % update statistics - BSE
+        %         if self.sd_dsc_mean_prev_sour == self.SD_DSC_MEAN_PREV_SOUR_BSE
+        %             x_bse_prev = x_bse;
+        %         end
+        %         if self.sd_dsc_var_prev_sour == self.SD_DSC_VAR_PREV_SOUR_BSE
+        %             v_bse_prev = v_bse;
+        %         end
+        %         % update statistics - DSC
+        %         v_dsc_prev = v_dsc;
+        %         % update statistics - DSC - instantaneous square error
+        %         ise_dsc_prev = ise_dsc;
+        % 
+        %         % soft symbol estimation
+        %         % take the detection value
+        %         if self.sd_out == self.SD_OUT_BSE
+        %             x = x_bse;
+        %         end
+        %         if self.sd_out == self.SD_OUT_DSC
+        %             x = x_dsc;
+        %         end
+        %         x(xdlocs) = self.symmap(x(xdlocs));
+        %         Xe = reshape(x, self.M, self.N).';
+        %     end
+        %     % only keep data part
+        %     x = x(xdlocs);
+        % end
+
+        %{
+        detect
+        @Y_DD:          the received signal in the delay Doppler domain [(batch_size), doppler, delay]
+        @lmax:          the maximal delay index
+        @kmax:          the maximal Doppler index
+        @No:            the noise (linear) power
+        @sym_map(opt):  false by default. If true, the output will be mapped to the constellation
+        %}
         function [x, H] = detect(self, Y_DD, lmax, kmax, No, varargin)
             % register optional inputs 
             inPar = inputParser;
@@ -260,19 +450,18 @@ classdef JPIC < handle
             xp = xp(:);
             xdlocs = self.XdLocs.';
             xdlocs = xdlocs(:);
+            xndlocs = ~xdlocs;
             
             % iterative detection
-            Xe = zeros(self.N, self.M);
             ise_dsc_prev = zeros(self.sig_len, 1);
             x_bse = zeros(self.sig_len, 1);
             v_bse = zeros(self.sig_len, 1);
             x_dsc = zeros(self.sig_len, 1);
             v_dsc = zeros(self.sig_len, 1);
-            x_det = zeros(self.sig_len, 1);
             for iter_id = 1:self.iter_num
                 % CE
-                X = Xe + self.Xp;
-                Phi = self.buildPhi(X, lmax, kmax);
+                Xe = reshape(x_dsc, self.M, self.N).';
+                Phi = self.buildPhi(Xe + self.Xp, lmax, kmax);
                 h = inv(Phi'*Phi)*Phi'*y;
                 % build the channel
                 H = self.buildHdd(h, lmax, kmax);
@@ -297,7 +486,7 @@ classdef JPIC < handle
                         otherwise
                             bso_zigma_1 = eye(x_num);
                     end
-                    x_bso = bso_zigma_1*(Hty - HtH_off*x_dsc);
+                    x_bso = bso_zigma_1*(Hty - HtH_off*x_dsc - HtH*xp);
                 else
                     % SD - BSO - mean - other iteration
                     switch self.sd_bso_mean_cal
@@ -306,7 +495,7 @@ classdef JPIC < handle
                         case self.SD_BSO_MEAN_CAL_LS
                             bso_zigma_n = inv(HtH);
                     end
-                    x_bso = bso_zigma_n*(Hty - HtH_off*x_dsc);
+                    x_bso = bso_zigma_n*(Hty - HtH_off*x_dsc - HtH*xp);
                 end
                 % SD - BSO - variance
                 switch self.sd_bso_var_cal
@@ -325,12 +514,14 @@ classdef JPIC < handle
                     v_bso = No.*bso_var_mat + HtH_off_sqr*v_dsc.*bso_var_mat_sqr;
                 end
                 v_bso = max(v_bso, self.min_var);
+                x_bso(xndlocs) = 0;
+                v_bso(xndlocs) = 0;
 
                 % SD - BSE
-                bse_x_bso_mat = repmat(x_bso(xdlocs), 1, self.constel_len);
-                bse_constel_mat = repmat(self.constel, self.data_len, 1) + repmat(xp(xdlocs), 1, self.constel_len);
                 % BSE - Estimate P(x|y) using Gaussian distribution
-                pxyPdfExpPower = -1./(2*v_bso(xdlocs)).*abs(bse_x_bso_mat - bse_constel_mat).^2;
+                bse_const_dist = repmat(x_bso, 1, self.constel_len) - repmat(self.constel, self.sig_len, 1);
+                pxyPdfExpPower = zeros(self.sig_len, self.constel_len);
+                pxyPdfExpPower(xdlocs, :) = -1./(2*v_bso(xdlocs)).*abs(bse_const_dist(xdlocs, :)).^2;
                 pxypdfExpNormPower = pxyPdfExpPower - max(pxyPdfExpPower, [], 2);   % make every row the max power is 0
                 pxyPdf = exp(pxypdfExpNormPower);
                 % BSE - Calculate the coefficient of every possible x to make the sum of all
@@ -339,10 +530,12 @@ classdef JPIC < handle
                 % BSE - PDF normalisation
                 pxyPdfNorm = pxyPdfCoeff.*pxyPdf;
                 % BSE - calculate the mean and variance
-                x_bse(xdlocs) = sum(pxyPdfNorm.*self.constel, 2);
-                x_bse_mat = repmat(x_bse(xdlocs), 1, self.constel_len);
-                v_bse(xdlocs) = sum(abs(x_bse_mat - self.constel).^2.*pxyPdfNorm, 2);
-                v_bse(xdlocs) = max(v_bse(xdlocs), self.min_var);
+                x_bse = sum(pxyPdfNorm.*self.constel, 2);
+                x_bse(xndlocs) = 0;
+                x_bse_mat = repmat(x_bse, 1, self.constel_len);
+                v_bse = sum(abs(x_bse_mat - self.constel).^2.*pxyPdfNorm, 2);
+                v_bse = max(v_bse, self.min_var);
+                v_bse(xndlocs) = 0;
                 % BSE - to original size
 
                 % SD - DSC
@@ -383,6 +576,8 @@ classdef JPIC < handle
                         v_dsc = (1 - rho_dsc).*v_dsc + rho_dsc.*v_bse;
                     end
                 end
+                x_dsc(xndlocs) = 0;
+                v_dsc(xndlocs) = 0;
 
                 % early stop
                 if iter_id > 1 && sum(abs(v_dsc - v_dsc_prev).^2) <= self.iter_diff_min
@@ -403,20 +598,14 @@ classdef JPIC < handle
                 ise_dsc_prev = ise_dsc;
 
                 % soft symbol estimation
-                % take the detection value
-                if self.sd_out == self.SD_OUT_BSE
-                    x = x_bse;
-                end
-                if self.sd_out == self.SD_OUT_DSC
-                    x = x_dsc;
-                end
-                x(xdlocs) = self.symmap(x(xdlocs));
-                Xe = reshape(x, self.M, self.N).';
+                x_dsc(xdlocs) = self.symmap(x_dsc(xdlocs));
             end
             % only keep data part
-            x = x(xdlocs);
+            x = x_bse(xdlocs);
         end
     end
+
+    
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Auxiliary Methods
