cfeb042c7762a59dfdd7dba6bfd16966deb8284d
textremo <wfwfpwefe2323@gmail.com>
2025-08-08 09:51:48 +0800

    clear

--- Tests/Whole/test_joint.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from whatshow_phy_mod_otfs import OTFS, OTFSResGrid, OTFSDetector
-from JPIC import JPIC
-eps = np.finfo(float).eps
-
-
--- init.m
+++
@@ -1,9 +0,0 @@
-clear;
-clc;
-
-% Add all paths
-addpath(genpath("Samples"));
-addpath(genpath("Tests"));
-addpath(genpath("Utils"));
-clear;
-clc;
